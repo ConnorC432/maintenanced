a53dfaa38325ddaef8b1ae4fc9f5bbb03ecb994f
AnorLondo <git@anorlondo.dev>
2026-06-01 22:40:33 +0100

    use sdbus-cxx to start update service and wait for start and finish signals

--- src/Packages/AptBackend.cpp
+++ src/Packages/AptBackend.cpp
@@ -6,9 +6,48 @@ All rights reserved.
 
 #include "AptBackend.h"
 
+#include <sdbus-c++/sdbus-c++.h>
+
+#include <iostream>
+
 namespace Packages {
 bool AptBackend::update() {
-  return system("apt update && apt upgrade -y") == 0;
+  try {
+    auto connection = sdbus::createSystemBusConnection();
+    auto systemd = sdbus::createProxy(
+      *connection,
+      sdbus::ServiceName{"org.freedesktop.systemd1"},
+      sdbus::ObjectPath{"/org/freedesktop/systemd1"});
+
+    std::cout << "Updating...\n";
+
+    systemd->callMethod("StartUpdate")
+    .onInterface("org.maintenanced.Updater1")
+    .withArguments("maintenance-apt.service", "replace");
+
+    auto proxy = sdbus::createProxy(
+      *connection,
+      sdbus::ServiceName{"org.maintenanced.Updater1"},
+      sdbus::ObjectPath{"/org/maintenanced/Updater1"});
+
+    proxy->uponSignal("Started")
+    .onInterface("org.maintenanced.Updater1")
+    .call([]() {
+      std::cout << "Started\n";
+    });
+
+    proxy->uponSignal("Finished")
+    .onInterface("org.maintenanced.Updater1")
+    .call([]() {
+      std::cout << "Finished\n";
+    });
+
+    connection->enterEventLoop();
+  } catch (const sdbus::Error& e) {
+    std::cerr << "Failed to connect to updater: " << e.what() << "\n";
+    return false;
+  }
+  return true;
 }
 std::string AptBackend::name() const { return "apt"; }
 }  // namespace Packages
--- src/Packages/PacmanBackend.cpp
+++ src/Packages/PacmanBackend.cpp
@@ -6,8 +6,49 @@ All rights reserved.
 
 #include "PacmanBackend.h"
 
+#include <sdbus-c++/sdbus-c++.h>
+
+#include <iostream>
+
 namespace Packages {
-bool PacmanBackend::update() { return system("pacman -Syyu --noconfirm") == 0; }
+bool PacmanBackend::update() {
+  try {
+    auto connection = sdbus::createSystemBusConnection();
+    auto systemd = sdbus::createProxy(
+      *connection,
+      sdbus::ServiceName{"org.freedesktop.systemd1"},
+      sdbus::ObjectPath{"/org/freedesktop/systemd1"});
+
+    std::cout << "Updating...\n";
+
+    systemd->callMethod("StartUpdate")
+    .onInterface("org.maintenanced.Updater1")
+    .withArguments("maintenance-pacman.service", "replace");
+
+    auto proxy = sdbus::createProxy(
+      *connection,
+      sdbus::ServiceName{"org.maintenanced.Updater1"},
+      sdbus::ObjectPath{"/org/maintenanced/Updater1"});
+
+    proxy->uponSignal("Started")
+    .onInterface("org.maintenanced.Updater1")
+    .call([]() {
+      std::cout << "Started\n";
+    });
+
+    proxy->uponSignal("Finished")
+    .onInterface("org.maintenanced.Updater1")
+    .call([]() {
+      std::cout << "Finished\n";
+    });
+
+    connection->enterEventLoop();
+  } catch (const sdbus::Error& e) {
+    std::cerr << "Failed to connect to updater: " << e.what() << "\n";
+    return false;
+  }
+  return true;
+}
 
 std::string PacmanBackend::name() const { return "pacman"; }
 
